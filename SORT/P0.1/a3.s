6f40cabecff470ed0e2f84edcd8ea592da4c301d
ELIOTFIGUER <33386179+eliitpyy@users.noreply.github.com>
2018-04-10 13:50:23 -0500

    Add files via upload

---
+++ SORT/P0.1/a3.s
@@ -0,0 +1,4 @@
+.section .text
+.global HERE
+HERE:
+	jmp HERE
---
+++ SORT/P0.1/holamundo.s
@@ -0,0 +1,12 @@
+section .text
+extern puts
+global main
+
+main:    
+   push dword msg                ;stash the location of msg on the stack.
+   call puts                     ;call the 'puts' routine (libc?) 
+   add esp, byte 4               ;clean the stack?
+   ret                           ;exit.
+
+msg:
+   db "Hello World!",0   
